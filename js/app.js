ff8b5b6b2033581f67b7be27a19ab0edbaf0f684
Jowelss <devejowels.gitl.loper@gmail.com>
2025-01-13 18:41:01 -0400

    change of functions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,46 @@
 const calculator = document.getElementById('main');
 const show = document.getElementById('show');
-const showResult = document.getElementById('show-result');
+const showResult = document.getElementById('result');
 
 const symbolsOp = {
   sumar: '+',
   restar: '-',
   multiplicar: '*',
   dividir: '/',
-  resultado: '=',
 };
 
-const resultArr = [];
+let result = [];
 
-function operar(item) {
-  // TOMAR EN CUENTA CAMBIAR EL METODO EVAL POR FUNCIONES QUE REALICEN LAS OPERACIONES QUE EVAL ME AHORRA
-  show.textContent += item;
-  showResult.textContent = eval(show.textContent);
+const indice = 0;
+
+let numbers = '';
+
+function operation(e) {
+  if (!isNaN(e)) {
+    numbers += e;
+  } else {
+    const number = parseFloat(numbers);
+
+    result.push(number, e);
+
+    numbers = '';
+  }
 }
+
+showResult.addEventListener('click', () => {
+  console.log(result);
+});
 
 calculator.addEventListener('click', (e) => {
   if (e.target.matches('.number')) {
     const number = e.target.textContent;
 
-    operar(number);
-  } else if (e.target.matches('.operator')) {
+    operation(number);
+  }
+
+  if (e.target.matches('.operator')) {
     const operator = symbolsOp[e.target.id];
 
-    operar(operator);
+    operation(operator);
   }
 });
